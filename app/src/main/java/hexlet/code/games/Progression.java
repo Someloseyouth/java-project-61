6f915fd12a74bb2e0547c3c4d08d2cd63b6e80ec
someloseyouth <someloseyouth@someloseyouth>
2023-04-22 20:50:17 +0300

    Add Progression.java

---
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -0,0 +1,51 @@
+package hexlet.code.games;
+
+import hexlet.code.Cli;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static hexlet.code.Cli.greet;
+import static hexlet.code.games.Even.getRandomNumber;
+
+
+public class Progression {
+
+    private static String point;
+
+    public static String randomProgression() {
+        int[] geometricProgression = new int[getRandomNumber(5, 10)];
+        int firstNumber = (getRandomNumber(1, 10));
+        int step = (getRandomNumber(1, 10));
+        int replaceSlot = getRandomNumber(geometricProgression.length, 1);
+        geometricProgression[0] = firstNumber;
+        for (int i = 1; i < geometricProgression.length; i++) {
+            geometricProgression[i] = geometricProgression[i - 1] + step;
+        }
+        point = String.valueOf((geometricProgression[replaceSlot]));
+        return Arrays.toString(geometricProgression).replace(point, "..");
+    }
+
+    public static void progressionGame() {
+        greet();
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            System.out.println("What number is missing in the progression?");
+            System.out.println("Question: " + randomProgression());
+            int correctAnswer = Integer.parseInt(point);
+            int answer = scan.nextInt();
+            System.out.println("Your answer: " + answer);
+            if (answer == correctAnswer) {
+                System.out.println("Correct!");
+                if (i == 2) {
+                    System.out.println("Congratulations, " + Cli.name + "!");
+                    System.exit(0);
+                }
+            } else {
+                System.out.println("'" + answer + "'" + " is wrong answer ;(. Correct answer was " + "'" + correctAnswer + "'" + ".");
+                System.out.println("Let's try again, " + Cli.name + "!");
+                System.exit(0);
+            }
+        }
+    }
+}
