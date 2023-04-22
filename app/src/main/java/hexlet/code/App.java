f371063a65d40441e622119df578ef23f4eb69ea
someloseyouth <someloseyouth@someloseyouth>
2023-04-22 20:40:47 +0300

    Changed App.java class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,19 +2,44 @@ package hexlet.code;
 
 import java.util.Scanner;
 
+import static hexlet.code.Cli.greet;
+import static hexlet.code.games.Calc.calcGame;
+import static hexlet.code.games.Even.evenGame;
+import static hexlet.code.games.GCD.GCD_Game;
+import static hexlet.code.games.Prime.primeGame;
+import static hexlet.code.games.Progression.progressionGame;
+
 public class App {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
         int choice = scan.nextInt();
         System.out.println("Your choice: " + choice);
         System.out.println();
-        if (choice == 1){
-          //  Cli cli = new Cli();
-            Cli.greet();
+        switch (choice) {
+            case 0:
+                break;
+            case 1:
+                greet();
+                break;
+            case 2:
+                evenGame();
+                break;
+            case 3:
+                calcGame();
+            case 4:
+                GCD_Game();
+            case 5:
+                progressionGame();
+            case 6:
+                primeGame();
         }
     }
 
