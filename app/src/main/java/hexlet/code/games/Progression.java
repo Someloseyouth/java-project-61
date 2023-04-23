c0bd3f322570b14726e93596fc1fda878a9a6f25
someloseyouth <someloseyouth@someloseyouth>
2023-04-23 17:58:54 +0300

    Changed method Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -2,7 +2,6 @@ package hexlet.code.games;
 
 import hexlet.code.Cli;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -10,9 +9,9 @@ import java.util.Scanner;
 
 public class Progression {
 
-    private static String point;
+    private static int correctAnswer;
 
-    public static String randomProgression() {
+    public static void randomProgression() {
         int[] geometricProgression = new int[Even.getRandomNumber(5, 10)];
         int firstNumber = (Even.getRandomNumber(1, 10));
         int step = (Even.getRandomNumber(1, 10));
@@ -21,17 +20,25 @@ public class Progression {
         for (int i = 1; i < geometricProgression.length; i++) {
             geometricProgression[i] = geometricProgression[i - 1] + step;
         }
-        point = String.valueOf((geometricProgression[replaceSlot]));
-        return Arrays.toString(geometricProgression).replace(point, "..");
-    }
+        for(int i=0;i<geometricProgression.length;i++){
+            if(i == replaceSlot){
+                System.out.print(".."+" ");
+                correctAnswer = geometricProgression[i];
+            }
+            else{
+                System.out.print(geometricProgression[i]+" ");
+            }
+        }
+        }
 
     public static void progressionGame() {
         Cli.greet();
         Scanner scan = new Scanner(System.in);
         for (int i = 0; i < 3; i++) {
             System.out.println("What number is missing in the progression?");
-            System.out.println("Question: " + randomProgression());
-            int correctAnswer = Integer.parseInt(point);
+            System.out.println("Question: " );
+            randomProgression();
+            System.out.println();
             int answer = scan.nextInt();
             System.out.println("Your answer: " + answer);
             if (answer == correctAnswer) {
